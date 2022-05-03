6225122f648a1593e2ee9219a58d13524fb30214
huesoluis <huesoluis@gmail.com>
2022-05-03 15:14:01 +0200

    modvac1

--- includes/head.php
+++ includes/head.php
@@ -22,6 +22,7 @@
     <link href="../css/easy-autocomplete.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
    	<link rel="stylesheet" href="css/tline.css">
+   	<link rel="stylesheet" href="config/global.css">
 
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-autocomplete/1.3.5/jquery.easy-autocomplete.min.js" type="text/javascript" ></script>
