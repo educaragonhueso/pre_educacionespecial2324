b5a885ec33b7d294e3093393ebcde0d3f3ca933c
huesoluis <huesoluis@gmail.com>
2023-05-08 13:56:04 +0200

    modprov

--- clases/models/Solicitud.php
+++ clases/models/Solicitud.php
@@ -2045,9 +2045,9 @@ as nasignado,c.nombre_centro, a.puntos_validados,a.id_centro_destino as id_centr
 	{
       $resultSet=array();
 		if($rol=='centro')
-		   $sql="SELECT * FROM alumnos a left join centros c on a.id_centro_final=c.id_centro JOIN baremo b ON b.id_alumno=a.id_alumno WHERE matricula='si' AND c.id_centro=$c AND estado_solicitud='apta' AND fase_solicitud!='borrador' AND tipo_inscripcion|='ordinaria' order by tipoestudios";
+		   $sql="SELECT * FROM alumnos a left join centros c on a.id_centro_final=c.id_centro JOIN baremo b ON b.id_alumno=a.id_alumno WHERE c.id_centro=$c AND estado_solicitud='apta' AND fase_solicitud!='borrador' AND tipo_inscripcion|='ordinaria' order by tipoestudios";
       else
-         $sql="SELECT * FROM alumnos a JOIN centros c ON a.id_centro_final=c.id_centro JOIN baremo b ON b.id_alumno=a.id_alumno WHERE matricula='si' AND estado_solicitud='apta' AND fase_solicitud!='borrador' AND tipo_inscripcion!='ordinaria' ORDER BY id_centro ";
+         $sql="SELECT * FROM alumnos a JOIN centros c ON a.id_centro_final=c.id_centro JOIN baremo b ON b.id_alumno=a.id_alumno WHERE estado_solicitud='apta' AND fase_solicitud!='borrador' AND tipo_inscripcion!='ordinaria' ORDER BY id_centro ";
 		$log->warning("CONSULTA MATRICULADOS FINAL SUBTIPO: ".$subtipo_listado);
 		$log->warning($sql);
 
